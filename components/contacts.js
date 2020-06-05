2ecd4d1632a1b955241c0f69c271f57bdbc79ab3
aristo85 <aritomsk@yahoo.com>
2020-06-05 21:47:24 +0700

    added featured projects

--- components/contacts.js
+++ components/contacts.js
@@ -11,41 +11,38 @@ class Contact extends Component {
                 <div className="section-title">
                     <div className="mt-n4 mb-n2" />
                 </div>
-                <Row className="container">
-                    <ListGroup className="w-100">
-                        <ListGroupItem color="info">
-                            <Row >
-                                <Col className="social"><FaPhoneSquareAlt/></Col>
-                                <Col className="m-auto"><p>+79234292313</p></Col>
-                            </Row>
-                        </ListGroupItem>
-                        <ListGroupItem color="info">
-                            <Row>
-                                <Col className="social"><FaEnvelopeSquare/></Col>
-                                <Col className="m-auto"><p>aritomsk@yahoo.com</p></Col>
-                            </Row>
-                        </ListGroupItem>
-                        <ListGroupItem color="info">
-                            <Row>
-                                <Col className="social"><FaSkype/></Col>
-                                <Col className="m-auto"><p>ari.rahim1</p></Col>
-                            </Row>
-                        </ListGroupItem>
-                    </ListGroup>
-                    <p className="container text-center">Если вам нравится мои работы и у вас есть идея проекта,
-                        звоните! илисвяжитесь со мной через социальные сайты, перечисленные ниже.</p>
-                    <div className="social">
-                        <a href="https://www.facebook.com/ari.rahim.56"
-                           rel="noopener noreferrer" target="_blank">
-                            <FaFacebookSquare/>
-                        </a>&nbsp;&nbsp;
-                        <a href="https://vk.com/id339599349"
-                           rel="noopener noreferrer" target="_blank">
-                            <FaVk/>
-                        </a>
-                    </div>
-
-                </Row>
+                <ListGroup className="w-50">
+                    <ListGroupItem color="info">
+                        <Row >
+                            <Col xs="3" className="social"><FaPhoneSquareAlt/></Col>
+                            <Col><p>+79234292313</p></Col>
+                        </Row>
+                    </ListGroupItem>
+                    <ListGroupItem color="info">
+                        <Row>
+                            <Col xs="3" className="social"><FaEnvelopeSquare/></Col>
+                            <Col><p>aritomsk@yahoo.com</p></Col>
+                        </Row>
+                    </ListGroupItem>
+                    <ListGroupItem color="info">
+                        <Row>
+                            <Col xs="3" className="social"><FaSkype/></Col>
+                            <Col><p>ari.rahim1</p></Col>
+                        </Row>
+                    </ListGroupItem>
+                </ListGroup>
+                <p className="container text-center">Если вам нравится мои работы и у вас есть идея проекта,
+                    звоните! илисвяжитесь со мной через социальные сайты, перечисленные ниже.</p>
+                <div className="container social text-center">
+                    <a href="https://www.facebook.com/ari.rahim.56"
+                       rel="noopener noreferrer" target="_blank">
+                        <FaFacebookSquare/>
+                    </a>&nbsp;&nbsp;
+                    <a href="https://vk.com/id339599349"
+                       rel="noopener noreferrer" target="_blank">
+                        <FaVk/>
+                    </a>
+                </div>
             </div>
         );
     }
