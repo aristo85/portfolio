c3b4ce651b8dbb2dcfb5bb8572f07a124180a23d
Ari Rahim <aritomsk@yahoo.com>
2020-12-12 17:19:09 +0700

    added 404 page

---
+++ pages/404.js
@@ -0,0 +1,15 @@
+export default function Custom404() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexFlow: 'column' ,
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <h1>404 - Page Not Found</h1>
+    </div>
+  );
+}
